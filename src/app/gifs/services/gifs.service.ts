8509a34ec41808fc7b4a76c98c8f8a3bb1881b61
Ivan <ivan.mondragon.sotres@gmail.com>
2023-10-26 20:05:48 -0600

    Save history search in localstorage

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -9,7 +9,9 @@ export class GifsService {
   private _apiKey: string = '1BLWyQMTIqxB1qBcWam16Cxj4SAcoFQQ';
   private _serviceUrl: string = 'https://api.giphy.com/v1/gifs';
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) {
+    this.loadLocalStorage();
+  }
   private organizeHistory(tag: string) {
     tag = tag.toLocaleLowerCase();
     if (this._tagsHistory.includes(tag)) {
@@ -17,6 +19,19 @@ export class GifsService {
     }
     this._tagsHistory.unshift(tag);
     this._tagsHistory = this.tagsHistory.splice(0, 10);
+    this.saveLocalStorage();
+  }
+
+  private saveLocalStorage(): void {
+    localStorage.setItem('history', JSON.stringify(this._tagsHistory));
+  }
+
+  private loadLocalStorage(): void {
+    if (!localStorage.getItem('history')) return;
+    this._tagsHistory = JSON.parse(localStorage.getItem('history')!);
+
+    if (this.tagsHistory.length === 0) return;
+    this.searchTag(this._tagsHistory[0]);
   }
   get tagsHistory() {
     return [...this._tagsHistory];
