004616e477034c2550ee33d5e91464dadaa17fdb
Ivan <ivan.mondragon.sotres@gmail.com>
2023-10-26 19:31:43 -0600

    Added service to request gifs

--- src/app/gifs/pages/home-page/home-page.component.ts
+++ src/app/gifs/pages/home-page/home-page.component.ts
@@ -1,9 +1,15 @@
 import { Component } from '@angular/core';
+import { GifsService } from '../../services/gifs.service';
+import { Gif } from '../../models/gifs.interface';
 
 @Component({
   selector: 'gifs-home-page',
-  templateUrl: './home-page.component.html'
+  templateUrl: './home-page.component.html',
 })
 export class HomePageComponent {
+  constructor(private gifsService: GifsService) {}
 
+  get gifs(): Gif[] {
+    return this.gifsService.gifList;
+  }
 }
--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,18 +1,42 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Gif, SearchResponse } from '../models/gifs.interface';
 
 @Injectable({ providedIn: 'root' })
 export class GifsService {
-  constructor() {}
-
+  public gifList: Gif[] = [];
   private _tagsHistory: string[] = [];
+  private _apiKey: string = '1BLWyQMTIqxB1qBcWam16Cxj4SAcoFQQ';
+  private _serviceUrl: string = 'https://api.giphy.com/v1/gifs';
 
+  constructor(private http: HttpClient) {}
+  private organizeHistory(tag: string) {
+    tag = tag.toLocaleLowerCase();
+    if (this._tagsHistory.includes(tag)) {
+      this._tagsHistory = this._tagsHistory.filter((oldtag) => oldtag !== tag);
+    }
+    this._tagsHistory.unshift(tag);
+    this._tagsHistory = this.tagsHistory.splice(0, 10);
+  }
   get tagsHistory() {
     return [...this._tagsHistory];
   }
 
   searchTag(tag: string): void {
-    this._tagsHistory.unshift(tag);
-    console.log(this._tagsHistory);
+    if (tag.length === 0) return;
+    this.organizeHistory(tag);
 
+    const params = new HttpParams()
+      .set('api_key', this._apiKey)
+      .set('limit', '10')
+      .set('q', tag);
+
+    this.http
+      .get<SearchResponse>(`${this._serviceUrl}/search`, { params })
+      .subscribe((resp) => {
+        console.log(resp);
+        this.gifList = resp.data;
+        console.log(this.gifList);
+      });
   }
 }
